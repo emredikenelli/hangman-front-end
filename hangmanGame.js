bc694d10701ee50f34e5465065e43ffb4a9270bb
emredikenelli <31349040+emredikenelli@users.noreply.github.com>
2018-08-28 17:20:12 +0300

    Add files via upload

--- hangmanGame.js
+++ hangmanGame.js
@@ -1,9 +1,11 @@
-console.log("Heyheyhey");
+var httpRequest;
+
 let btnArr = document.querySelectorAll(".alphabetSection button");
 for (var i = btnArr.length - 1; i >= 0; i--) {
 	let current = btnArr[i];
 	current.addEventListener("click", () => {
-    alert(current.innerText.charAt(0))
+		var req = {"letter" : current.innerText.charAt(0).toLowerCase()};
+    	makeRequest(JSON.stringify(req))
   });
 }
 
@@ -14,3 +16,55 @@ for (var i = btnArr2.length - 1; i >= 0; i--) {
     alert(current.innerText)
   });
 }
+
+function makeRequest(mv) {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = alertContents;
+    console.log(mv)
+    httpRequest.open('POST', 'http://localhost:9000/play');
+    httpRequest.setRequestHeader("Content-Type", "application/json;charset=UTF-8")
+    httpRequest.send(mv);
+
+  }
+
+  function alertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        getSecretWord();
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+
+  function getSecretWord() {
+    httpRequest = new XMLHttpRequest();
+
+    if (!httpRequest) {
+      alert('Giving up :( Cannot create an XMLHTTP instance');
+      return false;
+    }
+    httpRequest.onreadystatechange = secretWordAlertContents;
+    httpRequest.open('get', 'http://localhost:9000/secret_word');
+    httpRequest.send();
+
+  }
+
+  function secretWordAlertContents() {
+    if (httpRequest.readyState === XMLHttpRequest.DONE) {
+      if (httpRequest.status === 200) {
+        var res = JSON.parse(httpRequest.response);
+        var sw = document.getElementById('secretWord')
+        sw.innerText = res.secretWord;
+      } else {
+        alert('There was a problem with the request.');
+      }
+    }
+  }
+
+
